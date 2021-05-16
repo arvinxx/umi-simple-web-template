34f5ad109c70300194e0d5094a6629c7706333e7
arvinxx <arvinx@foxmail.com>
2021-05-16 21:20:51 +0800

    :sparkles: feat: 添加 api

---
+++ api/hello.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { name = 'World' } = request.query;
+  response.status(200).send(`Hello ${name}!`);
+};
