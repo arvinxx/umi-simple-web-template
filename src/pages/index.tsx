42ffade5354daabb896f6e65d2905539f5acbe08
arvinxx <arvinx@foxmail.com>
2021-06-06 20:11:21 +0800

    :art: chore: 修正模板

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import styles from './index.css';
+import styles from './index.less';
 
-export default function() {
+const App = () => {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
+          To get started, edit <code>src/pages/index.js</code> and save to reload.
+        </li>
+        <li>
+          <a href="https://umijs.org/guide/getting-started.html">Getting Started</a>
         </li>
       </ul>
     </div>
   );
-}
+};
+
+export default App;
